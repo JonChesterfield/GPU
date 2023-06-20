ac7e7e2cb354532da94ee75f2ba338593f3e90af
Jon Chesterfield <jonathanchesterfield@gmail.com>
2023-06-20 21:07:56 +0100

    refactor to drop gotos

--- token_allocate.cpp
+++ token_allocate.cpp
@@ -1,78 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "token_allocate.hpp"
 #include "thirdparty/EvilUnit/EvilUnit.h"
+#include "token_allocate.hpp"
 
+MAIN_MODULE() {
 
-MAIN_MODULE()
-{
-
-  TEST("allocate all then free all")
-    {
-      enum {N = 8};
-      token_allocate<N> a;
-      uint32_t vals[N];
-      for (unsigned i = 0 ; i < N; i++)
-        {
-          uint32_t value = a.alloc();
-          CHECK(i == value);
-          vals[i] = value;
-        }
-      for (unsigned i = 0 ; i < N; i++)
-        {
-          uint32_t value = vals[i];
-          a.free(value);
-        }
+  TEST("allocate all then free all") {
+    enum { N = 8 };
+    token_allocate<N> a;
+    uint32_t vals[N];
+    for (unsigned i = 0; i < N; i++) {
+      uint32_t value = a.alloc();
+      CHECK(i == value);
+      vals[i] = value;
     }
-  
-  TEST("init & walk once")
-    {
-      enum {N = 4};
-      token_allocate<N> a;
-      for (unsigned i = 0 ; i < N; i++)
-        {
-          uint32_t value = a.alloc();
-          CHECK(i == value);
-          a.free(value);
-        }
+    for (unsigned i = 0; i < N; i++) {
+      uint32_t value = vals[i];
+      a.free(value);
     }
+  }
 
-  TEST("init and walk twice")
-    {
-      enum {N = 8};
-      token_allocate<N> a;
-      for (unsigned i = 0 ; i < N; i++)
-        {
-          uint32_t value = a.alloc();
-          CHECK(i == value);
-          a.free(value);
-        }      
-      for (unsigned i = 0 ; i < N; i++)
-        {
-          uint32_t value = a.alloc();
-          CHECK(i == value);
-          a.free(value);
-        }      
+  TEST("init & walk once") {
+    enum { N = 4 };
+    token_allocate<N> a;
+    for (unsigned i = 0; i < N; i++) {
+      uint32_t value = a.alloc();
+      CHECK(i == value);
+      a.free(value);
     }
+  }
 
-  TEST("staggered alloc/free")
-    {
-      enum {N = 16};
-      token_allocate<N> a;
-      CHECK(a.alloc() == 0);
-      printf("alloc 0\n");
-      for (unsigned i = 1; i < N/2; i++)
-        {
-          uint32_t prev = (2*i-2);
-          uint32_t then = (2*i-1);
-          uint32_t x = a.alloc();
-          a.free(prev);
-          uint32_t y = a.alloc();
-          
-          CHECK(x == 2*i-1);
-          CHECK(y == 2*i);          
-          a.free(then);
-        }
+  TEST("init and walk twice") {
+    enum { N = 8 };
+    token_allocate<N> a;
+    for (unsigned i = 0; i < N; i++) {
+      uint32_t value = a.alloc();
+      CHECK(i == value);
+      a.free(value);
     }
+    for (unsigned i = 0; i < N; i++) {
+      uint32_t value = a.alloc();
+      CHECK(i == value);
+      a.free(value);
+    }
+  }
+
+  TEST("staggered alloc/free") {
+    enum { N = 16 };
+    token_allocate<N> a;
+    CHECK(a.alloc() == 0);
+    for (unsigned i = 1; i < N / 2; i++) {
+      uint32_t prev = (2 * i - 2);
+      uint32_t then = (2 * i - 1);
+      uint32_t x = a.alloc();
+      a.free(prev);
+      uint32_t y = a.alloc();
+
+      CHECK(x == 2 * i - 1);
+      CHECK(y == 2 * i);
+      a.free(then);
+    }
+  }
 }
--- token_allocate.hpp
+++ token_allocate.hpp
@@ -1,94 +1,88 @@
 #ifndef TOKEN_ALLOCATE_HPP
 #define TOKEN_ALLOCATE_HPP
 
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <assert.h>
 
-template <uint32_t N>
-struct token_allocate
-{
+// Somewhat esoteric allocator thing.
+// alloc() will return a unique value in [0, N)
+// free() will return that value to the pool
+// Encoding awkwardness is to use fetch_or instead of CAS to allocate
+// and to use a zero-initialised backing array
+
+template <uint32_t N> struct token_allocate {
   token_allocate() {
     _Static_assert(N != 0, "");
     _Static_assert(N != UINT32_MAX, "");
-    _Static_assert((N & (N - 1)) == 0, "N is power of two"); 
+    _Static_assert((N & (N - 1)) == 0, "N is power of two");
   }
 
-  uint32_t alloc() {
-    uint32_t index = guess_head_index();
-      
-  again:;
-    uint32_t before = __atomic_fetch_or(&data[index], 1, __ATOMIC_ACQ_REL);
-    if (!element_available(before)) {
-      index++;
-      index %= N;
-      goto again;
+  static void inc(uint32_t &x) { x = (x + 1u) % N; }
+
+  static uint32_t encode(uint32_t value, uint32_t index) {
+    return (value - index) << 1u;
+  }
+
+  static uint32_t decode(uint32_t value, uint32_t index) {
+    return index + (value >> 1u);
+  }
+
+  bool element_available(uint32_t v) { return (v & 1) == 0; }
+
+  void dump() {
+    printf("Head %u Tail %u N %u\n", head, tail, N);
+    for (unsigned i = 0; i < N; i++) {
+      printf("data[%u] = %u, encodes %u\n", i, data[i], i + (data[i] >> 1));
     }
-    // low bit is now set
-    assert(element_available(before));
-    assert(!index_available(index)); // but isn't now
-    return index + (before >> 1);
-  }
-  void free(uint32_t x) {
-    uint32_t index = guess_tail_index();
-  again:
-    uint32_t current = __atomic_load_n(&data[index], __ATOMIC_RELAXED);
-    if (element_available(current))
-      {
-        // Looking for a slot thaat doesn't have a value in it already
-        index++;
-        index %= N;
-        goto again;
-      }
-
-    // Got an entry with low bit clear
-    uint32_t propose = (x - index) << 1;
-    assert(!element_available(current));
-    assert(element_available(propose));
-    
-    bool succeeded = __atomic_compare_exchange_n(&data[index], &current, propose, true, __ATOMIC_ACQ_REL, __ATOMIC_RELAXED);
-    if (succeeded) {
-      return;
-    }
-    
-    uint32_t cas_got = current;
-    if (element_available(cas_got)) {
-      // Failed and there's something in the slot
-      index++;
-      index %= N;
-      goto again;
-    } else {
-      // Failed, but the slot doesn't have anything in it
-      goto again;
-    }
-  }
-
-  uint32_t guess_head_index() {
-    return __atomic_fetch_add(&head, 1, __ATOMIC_ACQ_REL) % N;
-  }
-
-  uint32_t guess_tail_index() {
-    return __atomic_fetch_add(&tail, 1, __ATOMIC_ACQ_REL) % N;
   }
   
+  uint32_t alloc() {
+    uint32_t index = __atomic_fetch_add(&head, 1, __ATOMIC_ACQ_REL) % N;
+
+    for (;;) {
+      uint32_t before = __atomic_fetch_or(&data[index], 1, __ATOMIC_ACQ_REL);
+      if (element_available(before)) {
+        return decode(before, index);
+      } else {
+        inc(index);
+      }
+    }
+  }
+
+  void free(uint32_t x) {
+    uint32_t index = __atomic_fetch_add(&tail, 1, __ATOMIC_ACQ_REL) % N;
+
+    for (;;) {
+      uint32_t current = __atomic_load_n(&data[index], __ATOMIC_RELAXED);
+      if (!element_available(current)) {
+        uint32_t propose = encode(x, index);
+        assert(element_available(propose));
+
+        // weak is fine
+        bool succeeded =
+            __atomic_compare_exchange_n(&data[index], &current, propose, true,
+                                        __ATOMIC_ACQ_REL, __ATOMIC_RELAXED);
+        if (succeeded) {
+          return;
+        }
+
+        uint32_t cas_got = current;
+        if (element_available(cas_got)) {
+          // Failed and there's something in the slot
+          inc(index);
+        } else {
+          // CAS failed, but the slot doesn't have anything in it, can try again
+        }
+      } else {
+        inc(index);
+      }
+    }
+  }
+
   uint32_t head = 0;
   uint32_t tail = 0;
   uint32_t data[N] = {0};
-
-  bool element_available(uint32_t v) {
-    return (v & 1) == 0;
-  }
-  bool index_available(uint32_t x) {
-    return element_available(__atomic_load_n(&data[x], __ATOMIC_RELAXED));
-  }
-  void dump()
-  {
-    printf("Head %u Tail %u N %u\n", head, tail, N);
-    for (unsigned i = 0; i < N; i++) {
-      printf("data[%u] = %u, encodes %u\n", i, data[i], i + (data[i]>>1));
-    }
-
-  }
 };
 
 #endif
